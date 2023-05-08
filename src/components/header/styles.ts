695d2e0a2ff0d6f4261ba12e44a701bf47bd23bf
Camila Mendes <mendemmcamila@gmail.com>
2023-05-07 23:43:08 -0300

    header

---
+++ src/components/header/styles.ts
@@ -0,0 +1,13 @@
+import { Grid } from '@mui/material';
+import styled from 'styled-components'
+
+export const NavBar = styled(Grid)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100px;
+  background-color: #669ED2;
+  color: #fff;
+  padding: 0 30px;
+`;
